8cfe6bd922fd3be578bae31628cc1e6f6a26d731
Eric Adams <eadams@synacor.com>
2012-08-30 13:01:02 -0400

    Initial push of grunt-bbb-proxy

---
+++ tasks/server.js
@@ -0,0 +1,147 @@
+/*
+ * Grunt Task File
+ * ---------------
+ *
+ * Task: Server
+ * Description: Serve the web application.
+ * Dependencies: express
+ *
+ */
+
+module.exports = function(grunt) {
+
+  // TODO: ditch this when grunt v0.4 is released
+  grunt.util = grunt.util || grunt.utils;
+
+  var _ = grunt.util._;
+  // Shorthand Grunt functions
+  var log = grunt.log;
+
+  grunt.registerTask("server", "Run development server.", function(prop) {
+    var options;
+    var props = ["server"];
+
+    // Keep alive
+    var done = this.async();
+
+    // If a prop was passed as the argument, use that sub-property of server.
+    if (prop) { props.push(prop); }
+
+    // Defaults set for server values
+    options = _.defaults(grunt.config(props) || {}, {
+      favicon: "./favicon.ico",
+      index: "./index.html",
+
+      port: process.env.PORT || 8000,
+      host: process.env.HOST || "127.0.0.1"
+    });
+
+    options.folders = options.folders || {};
+
+    // Ensure folders have correct defaults
+    options.folders = _.defaults(options.folders, {
+      app: "./app",
+      assets: "./assets",
+      dist: "./dist"
+    });
+
+    options.files = options.files || {};
+
+    // Ensure files have correct defaults
+    options.files = _.defaults(options.files, {
+      "app/config.js": "app/config.js"
+    });
+
+    // Run the server
+    grunt.helper("server", options);
+
+    // Fail task if errors were logged
+    if (grunt.errors) { return false; }
+
+    log.writeln("Listening on http://" + options.host + ":" + options.port);
+  });
+
+  grunt.registerHelper("server", function(options) {
+    // Require libraries.
+    var fs = require("fs");
+    var path = require("path");
+    var stylus = require("stylus");
+    var express = require("express");
+
+    // If the server is already available use it.
+    var site = options.server ? options.server() : express.createServer();
+
+    // Allow users to override the root.
+    var root = _.isString(options.root) ? options.root : "/";
+
+    // Process stylus stylesheets.
+    site.get(/.styl$/, function(req, res) {
+      var url = req.url.split("assets/css/")[1];
+      var file = path.join("assets/css", url);
+
+      fs.readFile(file, function(err, contents) {
+        var processer = stylus(contents.toString());
+
+        processer.set("paths", ["assets/css/"]);
+        processer.render(function(err, css) {
+          res.header("Content-type", "text/css");
+          res.send(css);
+        });
+      });
+    });
+
+    // Process proxy urls
+    site.get("/proxy", function(req, res, next) {
+      var url = require("url");
+      var http = require("http");
+      var proxy_url = decodeURIComponent(req.query.proxyto || "");
+      var proxy_opts = url.parse(proxy_url);
+
+      // Make the request
+      http.get(proxy_opts, function(http_res) {
+        var body = "";
+        http_res.setEncoding('utf8');
+        http_res.on('data', function(chunk) {
+          body += chunk;
+        });
+        http_res.on('end', function() {
+          res.json(body);
+        });
+        http_res.on('close', function(err) {
+          res.send(500, err);
+        });
+      });
+    });
+
+    // Map static folders.
+    Object.keys(options.folders).sort().reverse().forEach(function(key) {
+      site.get(root + key + "/*", function(req, res, next) {
+        // Find filename.
+        var filename = req.url.slice((root + key).length);
+
+        res.sendfile(path.join(options.folders[key] + filename));
+      });
+    });
+
+    // Map static files.
+    if (_.isObject(options.files)) {
+      Object.keys(options.files).sort().reverse().forEach(function(key) {
+        site.get(root + key, function(req, res) {
+          return res.sendfile(options.files[key]);
+        });
+      });
+    }
+
+    // Serve favicon.ico.
+    site.use(express.favicon(options.favicon));
+    
+    // Ensure all routes go home, client side app..
+    site.get("*", function(req, res) {
+      fs.createReadStream(options.index).pipe(res);
+    });
+
+    // Actually listen
+    site.listen(options.port, options.host);
+  });
+
+};
